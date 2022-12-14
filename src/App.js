b5bcc5afa91ed60047e32792bd0a4394e06695d2
jeremysacriz <114373649+jeremysacriz@users.noreply.github.com>
2022-12-14 20:43:46 +1100

    Comments + bug fixes - 14/12/22

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    - First argument is initial value (value passed in useState('...'))
    - Second argument is the function which changes the state of the UI
    */
-   const [color, setColor] = useState('black'); 
+   const [color, setColor] = useState('white'); 
    const [result, setResult] = useState();
 
    /* The function below is passed as an event handler, rather than a string. 
@@ -16,32 +16,41 @@ function App() {
    const handleSubmit = (e) => { 
       e.preventDefault() // cancels the default action that belongs to the event
 
-      let colorValue = e.target.username.value
+      let inputValue = e.target.username.value
+      let colorValue = inputValue.toLowerCase()
       let s = new Option().style;
       s.color = colorValue
 
-      let regex = /^#[0-9A-F]{6}$/i // this regex tests if the user input matches a specific hex code
-      let result = regex.test(colorValue) // returns true or false depending on user input
+      let regex1 = /^#[0-9A-F]{6}$/i // a regex is a sequence of characters that is used to match character combinations in strings
+      let result = regex1.test(colorValue) // searches for a match between regex1 & a specified string (in this case, a 6 digit hex code), returns true or false 
 
-      if (s.color === colorValue || result === true) {
+
+      let regex2 = /^#[0-9A-F]{3}$/i
+      let result2 = regex2.test(colorValue) // searches for a match between regex2 & a specified string (in this case, a 3 digit hex code), returns true or false
+
+      if (s.color === colorValue || result === true || result2 === true) {
          setColor(colorValue) // this function changes the state of the UI to the color that the user submitted
          setResult(true) // displays a message to the user 'This color is {colorValue}'
       } else {
-         console.log('Invalid color')
          setResult(false) // displays a message to the user 'Please enter a valid color'
       }
    }
 
    return (
       <section className="app">
+         <div className="app-title">
+            <h1>Color Checker</h1>
+         </div>
          <div className="app-container">
-            <div className="bg-color" style={{ background: color }}></div>
-            <form className="form" onSubmit={handleSubmit}>
-               <input className="input" type="text" placeholder="Enter a Hex code" name="username" autoComplete="off" />
-               <button className="submit" type="submit">Submit</button>
-            </form>
-            <div>{result === true && <div className="valid"></div>}</div>
-            <div>{result === false && <div className="invalid">Please enter a valid color.</div>}</div>
+            <div className="bg-container">
+               <div className="bg-color" style={{ background: color }}></div>
+               <form className="form" onSubmit={handleSubmit}>
+                  <input className="input" type="text" placeholder="Enter a color..." name="username" autoComplete="off" />
+                  <button className="submit" type="submit">Submit</button>
+               </form>
+               <div>{result === true && <div className="valid">This color is {color}.</div>}</div>
+               <div>{result === false && <div className="invalid">Please enter a hex code or valid color.</div>}</div>
+            </div>
          </div>
       </section>
    )
